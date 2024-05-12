9913da34257eb41c64559ba8e65d5e96a486c921
oladee <deemajor230600@gmail.com>
2024-05-12 23:47:03 +0100

    font setup, tailwind setup and browerRouter setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Goal from "./pages/Goal"
+
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>}/>
+        <Route path="/goal" element={<Goal/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Goal.jsx
@@ -0,0 +1,11 @@
+
+
+const Goal = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Goal

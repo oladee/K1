08cf91f85fed03a431920249cfc19ea7e242d656
oladee <deemajor230600@gmail.com>
2024-05-19 20:08:05 +0100

    doney

--- src/components/about/Mission.jsx
+++ src/components/about/Mission.jsx
@@ -10,7 +10,7 @@ const Mission = () => {
         <h1 className="font-SansationBold text-2xl md:text-[clamp(30px,2.77vw,40px)] md:leading-9 lg:leading-[52px] md:w-[70vw] lg:w-[53vw]">
         Preserving the rich legacy of yoruba music, our non-profit music foundation is dedicated to the preservation, promotion, and investment in the vibrant tapestry of Yoruba music, talents, research, and instruments
         </h1>
-        <p className="lg:w-[47.9vw] md:w-[70vw] text-base text-[clamp(18px,1.66vw,24px)]">
+        <p className="lg:w-[47.9vw] md:w-[70vw] text-base lg:text-[clamp(18px,1.66vw,24px)]">
         Through dynamic collaborations, we aspire to not only honor the rich heritage of Yoruba music but also to <span className="text-brown">empower emerging artists</span>, foster cultural exchange, and deepen global understanding of our cultural roots.
         </p>
       </div>
